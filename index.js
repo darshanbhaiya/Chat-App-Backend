3cbbfaf716ad9ae7ff88fdc599ad47fa5d466a98
Darshan Bhaiya <darshanbhaiya0403@gmail.com>
2024-06-24 14:26:19 +0530

    files added

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
-const express=require('express')
-const cors=require('cors')
-require('dotenv').config()
-const connectDB=require('./config/connectDB')
-const router=require('./routes/index')
-const cookiesParser=require("cookie-parser")
-const {app,server}=require('./socket/index')
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/connectDB');
+const router = require('./routes/index');
+const cookieParser = require('cookie-parser');
+const { app, server } = require('./socket/index');
 
-// const app=express()
+dotenv.config();
+
 app.use(cors({
     origin: process.env.FRONTEND_URL,
-    credentials:true
-}))
-app.use(express.json())
-app.use(cookiesParser())
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+}));
 
-const PORT=process.env.PORT || 8080
+app.use(express.json());
+app.use(cookieParser());
 
-app.get('/',(request,response)=>{
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (request, response) => {
     response.json({
-        message:"Server running at "+PORT
-    })
-})
-
-
-// api end points
-app.use('/api',router)
-
-
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
+        message: "Server running at " + PORT
     });
+});
 
+// API endpoints
+app.use('/api', router);
 
-
-connectDB().then(()=>{
-    server.listen(PORT,()=>{
-        console.log(`Server running at localhost:${PORT}`)
-    })
-})
-
+connectDB().then(() => {
+    server.listen(PORT, () => {
+        console.log(`Server running at localhost:${PORT}`);
+    });
+});
