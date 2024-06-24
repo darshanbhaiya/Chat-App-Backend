d391ffe44fe1536b466f6d25319fb89c64eb5f17
Darshan Bhaiya <darshanbhaiya0403@gmail.com>
2024-06-24 13:50:12 +0530

    files added

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ app.use('/api',router)
 
 // Handle CORS headers manually if needed
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
     res.header("Access-Control-Allow-Credentials", "true");
     if (req.method === 'OPTIONS') {
         res.sendStatus(200);
